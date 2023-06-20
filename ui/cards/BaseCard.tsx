f7cc3d37b6b54aec7fbe2d221c4b3a4d6f60b18d
Kasper UX <hello@kasper-ux.com>
2023-06-20 14:10:08 +0200

    updated font size

--- ui/cards/BaseCard.tsx
+++ ui/cards/BaseCard.tsx
@@ -20,7 +20,7 @@ const Container = styled.div`
 	padding: 1rem;
 	position: relative;
 	flex-direction: column;
-	width: 100%;
+	width: calc(100% - 2rem);
 	max-width: 20rem;
 	background: #ffffff;
 	box-shadow: rgba(0, 0, 0, 0.05) 0px 6px 24px 0px, rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;
